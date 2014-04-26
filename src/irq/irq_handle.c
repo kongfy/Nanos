05ff0074e38b90c73666db731a7e18db143c86c8
Fanyu Kong <njukongfy@gmail.com>
2014-04-26 20:45:31 +0800

    basic printf

--- src/irq/irq_handle.c
+++ src/irq/irq_handle.c
@@ -1,19 +1,17 @@
 #include "x86.h"
 #include "device.h"
+#include "stdio.h"
 
 void
 irq_handle(struct TrapFrame *tf) {
 	if (tf->irq == 1000) {
-		putchar('.');
+		printf(".");
 	} else if (tf->irq == 1001) {
 		uint32_t code = in_byte(0x60);
 		uint32_t val = in_byte(0x61);
 		out_byte(0x61, val | 0x80);
 		out_byte(0x61, val);
-		putchar('0' + code / 100);
-		putchar('0' + code / 10 % 10);
-		putchar('0' + code % 10);
-		putchar('\n');
+		printf("%d\n", code);
 	} else {
 		assert(0);
 	}
