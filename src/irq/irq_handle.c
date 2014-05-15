5e23d03475e60ca2b99d8ca366b41c46a4c5a216
Fanyu Kong <njukongfy@gmail.com>
2014-05-15 23:00:46 +0800

    irq #13 bug fix!

--- src/irq/irq_handle.c
+++ src/irq/irq_handle.c
@@ -4,9 +4,10 @@
 #include "kernel.h"
 
 void irq_handle(struct TrapFrame *tf) {
+	current->tf = tf;
+
 	if (tf->irq == 1000) {
-		current->tf = tf;
-	    schedule();
+		schedule();
 	} else if (tf->irq == 1001) {
 		uint32_t code = in_byte(0x60);
 		uint32_t val = in_byte(0x61);
@@ -14,7 +15,6 @@ void irq_handle(struct TrapFrame *tf) {
 		out_byte(0x61, val);
 		printf("%d\n", code);
 	} else if (tf->irq == 0x80) {
-		current->tf = tf;
 		schedule();
 	} else {
 		if (tf->irq == -1) {
