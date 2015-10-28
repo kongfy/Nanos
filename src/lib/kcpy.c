1b3179f1f4025a223f03105bf538d250af7e1c98
Fanyu Kong <njukongfy@gmail.com>
2015-10-28 23:44:41 +0800

    accelerate kcpy

--- src/lib/kcpy.c
+++ src/lib/kcpy.c
@@ -2,28 +2,47 @@
 #include "string.h"
 #include "memory.h"
 
+static inline
+int min (int a, int b)
+{
+    return a < b ? a : b;
+}
 
 uint32_t copy_from_kernel(Thread* pcb, void* dest, void* src, int len)
 {
-    int i = 0;
     uint32_t count = 0;
-    for (i = 0; i < len; ++i) {
-        char* pdest = (char*)translate_vaddr(pcb, (uint32_t)(dest++));
-        memcpy(pdest, src++, 1);
-        count++;
+    int l;
+
+    while (len > 0) {
+        char* pdest = (char*)translate_vaddr(pcb, (uint32_t)dest);
+        l = min(((((uint32_t)pdest >> 12) + 1) << 12) - (uint32_t)pdest, len);
+        memcpy(pdest, src, l);
+
+        len -= l;
+        dest += l;
+        src += l;
+        count += l;
     }
+
     return count;
 }
 
 uint32_t copy_to_kernel(Thread* pcb, void* dest, void* src, int len)
 {
-    int i = 0;
     uint32_t count = 0;
-    for (i = 0; i < len; ++i) {
-        char* psrc = (char*)translate_vaddr(pcb, (uint32_t)(src++));
-        memcpy(dest++, psrc, 1);
-        count++;
+    int l;
+
+    while (len > 0) {
+        char* psrc = (char*)translate_vaddr(pcb, (uint32_t)src);
+        l = min(((((uint32_t)psrc >> 12) + 1) << 12) - (uint32_t)psrc, len);
+        memcpy(dest, psrc, l);
+
+        len -= l;
+        dest += l;
+        src += l;
+        count += l;
     }
+
     return count;
 }
 
